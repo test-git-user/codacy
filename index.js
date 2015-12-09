0eafd2a245670e596e06da1d2a91aee64d8eb7cf
Corey Knight <corey.knight@learnosity.com>
2015-12-09 11:03:31 +1100

    added unused variable

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var settings = require('./package.json');
 var inAdapter = require(settings.inAdapter.type);
 var outAdapter = require('./lib/outAdapters/consoleAdapter');
 
+var unusedVariable = 3;
+
 var verbose = _.contains(process.argv.slice(2), '--verbose');
 var main = function(verbose) {
   var sites = inAdapter.getRunData();
